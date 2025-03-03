cbfdb68b74d0c8b7c585af99e2b9eb5f835f3144
kinfuy <kinfuy@outlook.com>
2025-03-03 23:20:42 +0800

    refactor: Remove deprecated Mind app role components and data files

---
+++ Apptune/Screen/Apps/Mind/RolesView.swift
@@ -0,0 +1,18 @@
+//
+//  Roles.swift
+//  Apptune
+//
+//  Created by 杨杨杨 on 2025/3/2.
+//
+
+import SwiftUI
+
+struct Roles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Roles()
+}
